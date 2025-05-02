c5317e9490b7f28bfea8549dcdf798d36b9e47c0
neriiacopo <neri.iacopo@gmail.com>
2025-05-02 23:53:23 +0200

    updated call to nested server

--- search.js
+++ search.js
@@ -23,7 +23,7 @@ function onRunButtonClick() {
         // const server = "http://localhost:5000";
 
         textEmbeds.then((text) => {
-            fetch(`${server}/semsegsearch`, {
+            fetch(`${server}/semsegsearch/search`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
